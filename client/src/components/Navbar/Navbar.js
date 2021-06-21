9802a3c11bfcc3eb9c8e040dcb28f207a9524076
teamDadUNODC <unodcteamdad@gmail.com>
2021-06-21 14:07:32 +0000

    Added moment and jwt-decode dependencies

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -1,13 +1,39 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { AppBar, Box, Toolbar, Button, Typography } from "@material-ui/core";
-import { Link, useLocation } from "react-router-dom";
+import { Link, useHistory, useLocation } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+import decode from 'jwt-decode';
 
+import * as actionType from '../../constants/actionTypes';
 import logo from "../../assets/Cinema-Logo.png";
 import useStyles from "./styles";
 
 const Navbar = () => {
-  const classes = useStyles();
+  const [user, setUser] = useState(JSON.parse(localStorage.getItem('profile')));
+  const dispatch = useDispatch();
   const location = useLocation();
+  const history = useHistory();
+  const classes = useStyles();
+
+  const logout = () => {
+    dispatch({ type: actionType.LOGOUT });
+
+    history.push('/auth');
+
+    setUser(null);
+  };
+
+  useEffect(() => {
+    const token = user?.token;
+
+    if (token) {
+      const decodedToken = decode(token);
+
+      if (decodedToken.exp * 1000 < new Date().getTime()) logout();
+    }
+
+    setUser(JSON.parse(localStorage.getItem('profile')));
+  }, [location]);
 
   return (
     <>
@@ -40,7 +66,7 @@ const Navbar = () => {
 
           <div className={classes.grow} />
 
-          {(location.pathname === "/" || location.pathname === "/watch" || location.pathname === "/get-ticketes" || location.pathname === "/premiere") && (
+          {(location.pathname === "/" || location.pathname === "/watch" || location.pathname === "/get-tickets" || location.pathname === "/premiere") && (
             <div className={classes.button}>
               <Button component={Link} to="/auth/signup" variant="contained" color="primary" disableElevation className={classes.menuButton}>
                 Sign Up
