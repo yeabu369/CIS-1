3ebd70951a3e2fe6c70f2dfc279fb9b7a477867d
yeabu369 <yeabsdebe@gmail.com>
2021-06-23 16:12:17 +0000

    Fixed bug on listItems

--- client/src/components/Dashboard/listItems.js
+++ client/src/components/Dashboard/listItems.js
@@ -1,7 +1,9 @@
 import React from "react";
-import ListItem from "@material-ui/core/ListItem";
-import ListItemIcon from "@material-ui/core/ListItemIcon";
-import ListItemText from "@material-ui/core/ListItemText";
+import { 
+  ListItem,
+  ListItemIcon,
+  ListItemText
+} from "@material-ui/core";
 import DashboardIcon from "@material-ui/icons/Dashboard";
 import ShoppingCartIcon from "@material-ui/icons/ShoppingCart";
 import PeopleIcon from "@material-ui/icons/People";
@@ -9,43 +11,45 @@ import BarChartIcon from "@material-ui/icons/BarChart";
 import LayersIcon from "@material-ui/icons/Layers";
 import TheatersIcon from '@material-ui/icons/Theaters';
 
+import { Link } from 'react-router-dom';
+
 export const mainListItems = (
-  <div>
-    <ListItem button>
-      <ListItemIcon>
-        <DashboardIcon />
-      </ListItemIcon>
-      <ListItemText primary="Dashboard" />
-    </ListItem>
-    <ListItem button>
-      <ListItemIcon>
-        <TheatersIcon />
-      </ListItemIcon>
-      <ListItemText primary="Movie Catalog" />
-    </ListItem>
-    <ListItem button>
-      <ListItemIcon>
-        <ShoppingCartIcon />
-      </ListItemIcon>
-      <ListItemText primary="Orders" />
-    </ListItem>
-    <ListItem button>
-      <ListItemIcon>
-        <PeopleIcon />
-      </ListItemIcon>
-      <ListItemText primary="Users" />
-    </ListItem>
-    <ListItem button>
-      <ListItemIcon>
-        <BarChartIcon />
-      </ListItemIcon>
-      <ListItemText primary="Reports" />
-    </ListItem>
-    <ListItem button>
-      <ListItemIcon>
-        <LayersIcon />
-      </ListItemIcon>
-      <ListItemText primary="Integrations" />
-    </ListItem>
-  </div>
+    <div>
+      <ListItem button component={Link} to="/dashboard/admin">
+        <ListItemIcon>
+          <DashboardIcon />
+        </ListItemIcon>
+        <ListItemText primary="Dashboard" />
+      </ListItem>
+      <ListItem button component={Link} to="/dashboard/movie-catalog">
+        <ListItemIcon>
+          <TheatersIcon />
+        </ListItemIcon>
+        <ListItemText primary="Movie Catalog" />
+      </ListItem>
+      <ListItem button component={Link} to="/dashboard/orders">
+        <ListItemIcon>
+          <ShoppingCartIcon />
+        </ListItemIcon>
+        <ListItemText primary="Orders" />
+      </ListItem>
+      <ListItem button component={Link} to="/dashboard/users">
+        <ListItemIcon>
+          <PeopleIcon />
+        </ListItemIcon>
+        <ListItemText primary="Users" />
+      </ListItem>
+      <ListItem button component={Link} to="/dashboard/reports">
+        <ListItemIcon>
+          <BarChartIcon />
+        </ListItemIcon>
+        <ListItemText primary="Reports" />
+      </ListItem>
+      <ListItem button component={Link} to="/dashboard/integrations">
+        <ListItemIcon>
+          <LayersIcon />
+        </ListItemIcon>
+        <ListItemText primary="Integrations" />
+      </ListItem>
+    </div>
 );
