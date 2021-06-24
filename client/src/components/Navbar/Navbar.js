ae267fbaa824ba0338eff933b8f4cd2b0ac8c262
yeabu369 <yeabsdebe@gmail.com>
2021-06-24 20:06:35 +0300

    Fixed navbar bug

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -18,7 +18,7 @@ const Navbar = () => {
   const logout = () => {
     dispatch({ type: actionType.LOGOUT });
 
-    history.push('/auth');
+    history.push('/auth/signin');
 
     setUser(null);
   };
@@ -68,7 +68,7 @@ const Navbar = () => {
 
           {user?.result ? (
             <div className={classes.profile}>
-              <Avatar className={classes.purple} alt={user?.result.name} src={user?.result.imageUrl}>{user?.result.name.charAt(0)}</Avatar>
+              <Avatar className={classes.purple} alt={user?.result.FullName} src={user?.result.imageUrl}>{user?.result.FullName.charAt(0)}</Avatar>
               <Typography className={classes.userName} variant="h6">{user?.result.name}</Typography>
               <Button variant="contained" className={classes.logout} color="secondary" onClick={logout}>Logout</Button>
             </div>
